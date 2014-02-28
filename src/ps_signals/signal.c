dcf0ca76bd13d77a6acbe8a90ecf029eda11f5ce
HerculesShek <hercules.shek@gmail.com>
2014-02-28 17:42:30 +0800

    add math_test.c

--- src/ps_signals/signal.c
+++ src/ps_signals/signal.c
@@ -8,6 +8,10 @@ void die(int sig)
   puts("\ndeal with something, like close file and network connection...");
   exit(1);
 }
+void coffee(int sig)
+{
+  puts("\ntime for coffee...\n");
+}
 //  register a handler.
 int catch_signal(int sig, void (*handler)(int))
 {
@@ -25,8 +29,9 @@ int catch_signal(int sig, void (*handler)(int))
 
 int main()
 {
+  catch_signal(SIGALRM, coffee);
   catch_signal(SIGINT, die);
-
+  alarm(3);
   char name[30];
   printf("Enter your name: ");
   fgets(name, 30, stdin);
