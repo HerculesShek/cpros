0a978ab0444991fd4028a15efa799cde246e9d15
HerculesShek <hercules.shek@gmail.com>
2014-02-28 16:17:23 +0800

    add signal.c

---
+++ src/ps_signals/signal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+// handler for interrupt
+void die(int sig)
+{
+  puts("\ndeal with something, like close file and network connection...");
+  exit(1);
+}
+//  register a handler.
+int catch_signal(int sig, void (*handler)(int))
+{
+  struct sigaction action;
+  action.sa_handler = handler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  int res = sigaction(sig, &action, NULL);
+  if(res==-1){
+	fprintf(stderr, "Can't map the handler");
+	exit(2);
+  }
+  return res;
+}
+
+int main()
+{
+  catch_signal(SIGINT, die);
+
+  char name[30];
+  printf("Enter your name: ");
+  fgets(name, 30, stdin);
+  printf("Hello %s\n", name);
+  return 0;
+} 
